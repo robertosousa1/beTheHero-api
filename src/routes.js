e096c47db1973f7f9e6f754292a260e0c72e89de
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-28 14:13:32 -0300

    rota para listagem e criação de incidentes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,12 @@ const { Router } = require('express');
 const routes = Router();
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
 
 module.exports = routes;
