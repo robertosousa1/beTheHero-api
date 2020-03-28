8522e14955a6a7d0c56da427f9f729ff1adddf97
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-28 12:00:32 -0300

    configurando server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(process.env.PORT || 3333);
