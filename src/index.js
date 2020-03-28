4a80675093eb92214fd55889e13a156345b4270d
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-28 15:13:33 -0300

    config server com cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
